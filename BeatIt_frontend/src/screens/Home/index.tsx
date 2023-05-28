cca4e6a14c0558d66b22400b1b885e434fbe1ded
Amy <amylingyuhui@gmail.com>
2023-05-28 19:19:58 +0800

    added backend folder

--- BeatIt_frontend/src/screens/Home/index.tsx
+++ BeatIt_frontend/src/screens/Home/index.tsx
@@ -1,12 +1,9 @@
 import React, {ReactElement} from 'react';
-import {Text} from 'react-native';
-import Container from '../../components/common/Container';
+
 import HomeComponent from '../../components/Home';
 
 const Home: () => ReactElement = () => {
-  return (
-  <HomeComponent />
-  );
+  return <HomeComponent />;
 };
 
 export default Home;
