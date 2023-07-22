73e3c2ab75a5c6c87e4d92c06255db39ab1cefa1
Amy <amylingyuhui@gmail.com>
2023-07-22 13:32:11 +0800

    small changes

--- frontend/src/navigations/index.tsx
+++ frontend/src/navigations/index.tsx
@@ -1,31 +1,28 @@
-import React, { ReactElement, useContext } from 'react';
-import {
-    NavigationContainer
-} from '@react-navigation/native';
+import React, {ReactElement, useContext} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import HomeTab from './HomeTab';
 import AuthStack from './AuthStack';
-import { Header, createStackNavigator } from '@react-navigation/stack';
-import { AuthContext } from '../context/providers/authProvider';
-import { useColorScheme } from 'react-native';
+import {Header, createStackNavigator} from '@react-navigation/stack';
+import {AuthContext} from '../context/providers/authProvider';
+import {useColorScheme} from 'react-native';
 import colors from '../assets/themes/colors';
 import darkColors from '../assets/themes/darkColors';
 
 type AppNavProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const stack = createStackNavigator();
 
 const AppNavContainer: (props: AppNavProps) => ReactElement = () => {
-    const authContext = useContext(AuthContext);
-    const theme = useColorScheme();
+  const authContext = useContext(AuthContext);
+  const theme = useColorScheme();
 
-
-    return (
-        <NavigationContainer theme={theme === 'dark' ? darkColors : colors}>
-            {authContext.isLoggedIn ? <HomeTab /> : <AuthStack />}
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer theme={theme === 'dark' ? darkColors : colors}>
+      {authContext.isLoggedIn ? <HomeTab /> : <AuthStack />}
+    </NavigationContainer>
+  );
 };
 
 export default AppNavContainer;
--- frontend/src/screens/PlaylistCreation/index.tsx
+++ frontend/src/screens/PlaylistCreation/index.tsx
@@ -1,4 +1,5 @@
-import React, {ReactElement} from 'react';
+import {ReactElement} from 'react';
+import * as React from 'react';
 import Container from '../../components/common/Container';
 import PlaylistComponent from '../../components/Playlist/PlaylistCreation';
 
